2ee6ed441cb66fa232197789983347e1d82b69d1
Zhuyi731 <604032172@qq.com>
2019-03-29 14:15:13 +0800

    添加购物车部分代码

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,15 +1,64 @@
-//logs.js
-const util = require('../../utils/util.js')
+const app = getApp();
 
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+    data: {
+        carts: [{
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }, {
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }, {
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }, {
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }, {
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }, {
+            "src": "/images/top/top1.jpg",
+            "title": "Nike Nike Air Mag到未来Air Mag",
+            "price": "79999",
+            "place": "北京",
+            "description": "还记得儿时我们都很喜欢走起路来又发光又有声音的鞋吗？而现在，耐克为我们带来了这样经典的回忆。并且，不只有回忆，更有逼格满满的黑科技。",
+            count: 1
+        }],
+        totalPrice: 0
+    },
+    onLoad: function() {
+        let carts = wx.getStorageSync("cart") || [];
+
+        wx.request({
+            url: app.url("/data/goods.json"),
+            success: res => {
+              
+            }
+        });
+
+    },
+    nothing:function(e){
+      debugger;
+    }
 })
